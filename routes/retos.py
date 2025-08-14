7748eb9c5150e2c40ca1d01e6e55f1ea0758faf5
DeikerJ <sdrosn@gmail.com>
2025-08-14 13:13:19 -0600

    los errores se acaban

--- routes/retos.py
+++ routes/retos.py
@@ -26,9 +26,10 @@ async def post_reto(
     return await create_reto(reto)
 
 # GET /retos/{id}
-@router.get("/retos/{id}")
+@router.get("/{id}")
 async def get_reto(id: str):
     return await get_reto_by_id(id)
+
 
 # PUT /retos/{id}
 @router.put("/{id}")
@@ -41,18 +42,11 @@ async def delete_reto_endpoint(id: str):
     await delete_reto(id)
     return {"mensaje": "Reto eliminado correctamente"}
 
+ 
 # GET /retos
-#@router.get("/retos")
-#async def get_misr(user: dict = Depends(validate_token)):
- #  return await get_ret_os_by_usuario(user["id"])
-
-@router.get("/retos", response_model=List[Retos])
+@router.get("/", response_model=List[Retos])
 async def get_retos(
-    usuario_id: Optional[str] = Query(
-        None, description="Filtra por ID de usuario"
-    ),
-    categoria_id: Optional[str] = Query(
-        None, description="Filtra por ID de categoría"
-    )
+    usuario_id: Optional[str] = Query(None),
+    categoria_id: Optional[str] = Query(None)
 ):
     return await listar_retos(usuario_id, categoria_id)
